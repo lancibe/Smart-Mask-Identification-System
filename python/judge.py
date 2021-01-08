aa90d275c05d9d0e9fcb1c36ce9055473c321374
lancibe <1609547089@qq.com>
2021-01-08 15:22:39 +0800

    update

--- python/judge.py
+++ python/judge.py
@@ -5,21 +5,28 @@ Created on Mon Jan  4 10:04:47 2021
 @author: Lancibe
 """
 import cv2
-def judge(name):
+
+def judge(imagedir):
+    imagedir='E:\\projects\\Smart_Mask_Identification_System\\data\\img\\'+imagedir
+    img=cv2.imread(imagedir)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 图像灰化
+    
     detector= cv2.CascadeClassifier('E:\\projects\\Smart_Mask_Identification_System\\haarcascade_frontalface_default.xml')
     mask_detector=cv2.CascadeClassifier('E:\\projects\\Smart_Mask_Identification_System\\bin\\xml\\cascade.xml')
-     
-    img = cv2.imread('E:\\projects\\Smart_Mask_Identification_System\\data\\img\\'+str(name))
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = detector.detectMultiScale(gray, 1.1, 3)
-    for (x, y, w, h) in faces:
-        #参数分别为 图片、左上角坐标，右下角坐标，颜色，厚度
-        #face=img[y:y+h,x:x+w]  # 裁剪坐标为[y0:y1, x0:x1]
-        mask_face=mask_detector.detectMultiScale(gray, 1.1, 5)
-        for (x2,y2,w2,h2) in mask_face:
-            cv2.rectangle(img, (x2, y2), (x2 + w2, y2 + h2), (0, 0, 255), 2)
-     
-    cv2.imshow('Cheney', img)
-     
+    faces = detector.detectMultiScale(gray, 1.01, 4)  # 识别人脸  1.02  每次检测的放大倍率与被检查物体成像大小相关
+    masks = mask_detector.detectMultiScale(gray, 1.01, 3)  # 识别人脸  1.02  )
+    print( "faces num= " + str(len(faces)) + "masks num= " + str(len(masks)))
+    
+    if(len(faces) | len(masks)):
+         for (x, y, w, h) in faces:
+             img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)  # 在人脸区域画一个正方形出来
+             img = cv2.putText(img , "nomask", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2) 
+             
+         for (mx, my,mw, mh) in masks:
+             img = cv2.rectangle(img, (mx, my), (mx + mw, my + mh), (0, 255, 0), 2)  # 在戴口罩人脸区域画一个正方形出来
+             img = cv2.putText(img , "mask", (mx,my), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2) #在戴口罩人脸区域输出字符
+    
+    
+    cv2.imshow('result',img)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
-    input()
