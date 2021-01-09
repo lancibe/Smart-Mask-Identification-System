ce30af15ba7cf5480e2689cd4d5688347b77b1ff
lancibe <1609547089@qq.com>
2021-01-09 21:53:40 +0800

    update

--- python/judge.py
+++ python/judge.py
@@ -9,22 +9,22 @@ import cv2
 def judge(imagedir):
     imagedir='E:\\projects\\Smart_Mask_Identification_System\\data\\img\\'+imagedir
     img=cv2.imread(imagedir)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 图像灰化
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # gray process
     
     detector= cv2.CascadeClassifier('E:\\projects\\Smart_Mask_Identification_System\\haarcascade_frontalface_default.xml')
     mask_detector=cv2.CascadeClassifier('E:\\projects\\Smart_Mask_Identification_System\\bin\\xml\\cascade.xml')
-    faces = detector.detectMultiScale(gray, 1.01, 4)  # 识别人脸  1.02  每次检测的放大倍率与被检查物体成像大小相关
-    masks = mask_detector.detectMultiScale(gray, 1.01, 3)  # 识别人脸  1.02  )
+    faces = detector.detectMultiScale(gray, 1.01, 4)  # identify faces
+    masks = mask_detector.detectMultiScale(gray, 1.01, 3)  # identify masks
     print( "faces num= " + str(len(faces)) + "masks num= " + str(len(masks)))
     
     if(len(faces) | len(masks)):
          for (x, y, w, h) in faces:
-             img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)  # 在人脸区域画一个正方形出来
+             img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)  # draw a square in face area
              img = cv2.putText(img , "nomask", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2) 
              
          for (mx, my,mw, mh) in masks:
-             img = cv2.rectangle(img, (mx, my), (mx + mw, my + mh), (0, 255, 0), 2)  # 在戴口罩人脸区域画一个正方形出来
-             img = cv2.putText(img , "mask", (mx,my), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2) #在戴口罩人脸区域输出字符
+             img = cv2.rectangle(img, (mx, my), (mx + mw, my + mh), (0, 255, 0), 2)  # draw a square in mask area
+             img = cv2.putText(img , "mask", (mx,my), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2) # print characters
     
     
     cv2.imshow('result',img)
--- python/processImages.py
+++ python/processImages.py
@@ -6,24 +6,24 @@ Created on Sun Jan  3 12:03:46 2021
 """
 import pandas as pd
 import cv2
-names=pd.read_excel('E:\\projects\\Smart_Mask_Identification_System\\data\\pos.xlsx')['names']# 读取所有照片名字
-i=100000 #用于重新命名
+names=pd.read_excel('E:\\projects\\Smart_Mask_Identification_System\\data\\pos.xlsx')['names'] # read all names of photos
+i=100000 # rename
 j=0
 for imagepath in names:
-    #读取图片
+    # read photos
     img = cv2.imread(imagepath)
-    #转成灰度
+    # gray process
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #人脸识别器
+    # identify faces
     detector = cv2.CascadeClassifier('E:\\projects\\Smart_Mask_Identification_System\\haarcascade_frontalface_default.xml')
-    #获取人脸位置
+    # obtain the coordinates
     faces = detector.detectMultiScale(gray, 1.1, 5)
     for (x, y, w, h) in faces:
-        #裁减图片
-        gray = gray[y:y+h,x:x+w]  # 裁剪坐标为[y0:y1, x0:x1]
-        #如果人脸不为空
+        # cut pics
+        gray = gray[y:y+h,x:x+w]  # cutting coordinates are [y0:y1, x0:x1]
+        # if has face
         try:
-            # 保存裁减后的灰度图
+            # save after-processing pics
             cv2.imwrite('E:\\projects\\Smart_Mask_Identification_System\\data\\After_Processed\\'+str(i)+'.jpg', gray)
             # cv2.waitKey(3000)
             i += 1
